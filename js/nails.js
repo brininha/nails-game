53f63db286d3ea35db7c5df3e583ef3a569d9aa2
Sabrina Cristan <sabrina.cristan@gmail.com>
2025-07-22 20:19:36 -0300

    atualizando design dos esmaltes

--- js/nails.js
+++ js/nails.js
@@ -8,19 +8,20 @@ let som = new Audio();
 som.src = "https://www.soundjay.com/buttons/sounds/button-7.mp3";
 
 const esmaltes = [
-    {img: 'e1.png'},
-    {img: 'e2.png'},
-    {img: 'e3.png'},
-    {img: 'e4.png'},
-    {img: 'e5.png'},
-    {img: 'e6.png'},
-    {img: 'e7.png'},
-    {img: 'e8.png'},
-    {img: 'e9.png'},
-    {img: 'e10.png'},
-    {img: 'e11.png'},
-    {img: 'e12.png'},
-    {img: 'e13.png'},
+    {img: 'e1.png', nome: 'Noite estrelada'},
+    {img: 'e2.png', nome: 'Café com leite'},
+    {img: 'e3.png', nome: 'Algodão doce'},
+    {img: 'e4.png', nome: 'Maçã do amor'},
+    {img: 'e5.png', nome: 'Fusca azul'},
+    {img: 'e6.png', nome: 'Abacate'},
+    {img: 'e7.png', nome: 'Fúria da noite'},
+    {img: 'e8.png', nome: 'Xote da alegria'},
+    {img: 'e9.png', nome: 'Coração indomável'},
+    {img: 'e10.png', nome: 'Magia de unicórnio'},
+    {img: 'e11.png', nome: 'Sapatinhos de rubi'},
+    {img: 'e12.png', nome: 'Jerimum'},
+    {img: 'e13.png', nome: 'Cisne negro'},
+    {img: 'e14.png', nome: 'Nevou'},
 ]
 
 const esmalteGrid = document.querySelector('.caixa-esmaltes');
@@ -29,14 +30,15 @@ esmaltes.forEach((esmalte, index) => {
     esmalteDiv.classList.add('esmalte');
     esmalteDiv.innerHTML = `<img src="./images/esmaltes/${esmalte.img}">`;
     esmalteDiv.addEventListener('click', () => {
-        const nailGrid = document.querySelector('.caixa-unha');
-        nailGrid.innerHTML = `<img src="./images/unhas/${esmalte.img}">`;
+        const nailDiv = document.querySelector('.unha-escolhida');
+        nailDiv.innerHTML = `<img src="./images/unhas/${esmalte.img}">`;
         sessionStorage.setItem('selectedNail', esmalte.img);
+        sessionStorage.setItem('selectedColor', esmalte.nome);
     });
     esmalteGrid.appendChild(esmalteDiv);
 });
 
-const btnNext = document.querySelector('.btn-next');
+const btnNext = document.querySelector('.btn-prox');
 btnNext.addEventListener('click', () => {
     som.play();
     som.onended = () => {
